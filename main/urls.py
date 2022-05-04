bc11cce174899ccc23a42b5414a66219286dbd55
Cauã Henrique Viana Salgado <cauahenrique230503@gmail.com>
2022-05-04 11:08:32 -0300

    Statics
    Incluindo arquivos estáticos css, img, js

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import index, sobre, contato, quests
+from .views import index, quests
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('sobre/', sobre, name='sobre'),
-    path('contato/', contato, name='contato'),
     path('quests/', quests, name='quests')
 ]
--- main/views.py
+++ main/views.py
@@ -4,13 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html')
 
-
-def sobre(request):
-    return render(request, 'main/sobre.html')
-
-
-def contato(request):
-    return render(request, 'main/contato.html')
 def quests(request):
     if request.session.get('usuario'):
         return HttpResponse('Logado')
