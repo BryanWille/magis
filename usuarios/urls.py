877cce9a6bd71d4d0d0cdebd1c7e3949c0b63f53
Caua Henrique <cauahenrique230503@gmail.com>
2022-06-19 01:04:27 -0300

    Revert "Merge branch 'backend' into frontend"
    This reverts commit be9830c491c0d8d5b400920ff48c07cd32a68ce4, reversing
    changes made to a7623b4561bfc7e27c0b0bbc93c0406ae4e9de6a.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,14 +2,9 @@ from django.urls import path
 from .views import cadastro, login, valida_cadastro, valida_login, sair
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("login/", login1, name="login1"),
-    path("cadastro/", cadastro, name="cadastro")
-=======
     path("login/", login, name="login1"),
     path("cadastro/", cadastro, name="cadastro"),
     path("valida_cadastro", valida_cadastro, name="valida_cadastro"),
     path("valida_login", valida_login, name="valida_login"),
     path("sair/", sair, name="sair")
->>>>>>> parent of c5ed203 (Login com allauth funcionando)
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,28 +1,6 @@
 from django.forms import EmailField
 from django.shortcuts import render
 
-<<<<<<< HEAD
-def login1(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-        
-        user = User.objects.filter(email = email).first()
-        if not user:
-            message = {'error': 'Usuário não existe!'}
-            context = message
-            return render(request, "usuarios/login.html", context)
-        user = authenticate(username=email, password=senha)
-        print(user)
-        if user is not None:
-            login(request, user)
-            redirect('')
-        else:
-            message = {'error': 'Email ou senha incorreto(s)!'}
-            context = message
-            return render(request, "usuarios/login.html", context)
-    return render(request, "usuarios/login.html")
-=======
 from .models import Usuario
 from django.shortcuts import redirect
 from hashlib import sha256
@@ -31,7 +9,6 @@ from hashlib import sha256
 def login(request):
     status = request.GET.get('status')
     return render(request, "usuarios\login.html", {"status":status})
->>>>>>> parent of c5ed203 (Login com allauth funcionando)
 
 def cadastro(request):
     status = request.GET.get('status')
@@ -57,11 +34,6 @@ def valida_cadastro(request):
         senha = sha256(senha.encode()).hexdigest()
         usuario = Usuario(nome=nome, email=email, senha=senha)
         usuario.save()
-<<<<<<< HEAD
-        message = {'success': 'Usuário cadastrado com sucesso!'}
-        context = message
-    return render(request, "usuarios/cadastro.html", context)
-=======
         return redirect('/auth/cadastro/?status=0')
         
     except:
@@ -84,5 +56,4 @@ def valida_login(request):
 def sair(request):
     request.session.flush()
     return redirect('/auth/login')
->>>>>>> parent of c5ed203 (Login com allauth funcionando)
 
