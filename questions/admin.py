be24e4ddef1f9e263498d079626c153d5c957021
Cauã Henrique Viana Salgado <cauahenrique230503@gmail.com>
2022-06-23 11:25:13 -0300

    Admins
    Adicionei os admins

--- questions/admin.py
+++ questions/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+from . import models
 
-admin.site.register(Course)
+class TeacherAdminArea(admin.AdminSite):
+    site_header = "Teacher Database"
+
+class StudentAdminArea(admin.AdminSite):
+    site_header = "Student Database"
 
 class AnswerAdmin(admin.StackedInline):
     model = Answer
@@ -12,6 +17,18 @@ class AnswerAdmin(admin.StackedInline):
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [AnswerAdmin]
 
+admin.site.register(Course)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
 admin.site.register(ScoreBoard)
+
+teacher_site = TeacherAdminArea(name='TeacherAdmin')
+teacher_site.register(Course)
+teacher_site.register(Question, QuestionAdmin)
+teacher_site.register(Answer)
+
+student_site = StudentAdminArea(name='StudentAdmin')
+student_site.register(Question, QuestionAdmin)
+
+
+
